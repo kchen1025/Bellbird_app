b5779c4fcf67192328f1a52526112ce80129850e
Kevin Chen <kchen1025@gmail.com>
2017-01-09 11:41:20 -0800

    added some upvote functions

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -4,19 +4,39 @@ class AlarmsController < ApplicationController
 		@alarm = Alarm.all
 	end
 
+
 	def new
 		@alarm = Alarm.new
 	end
 
 	def create
 		@alarm = Alarm.new(alarm_params)   
-	if @alarm.save
-	  # Handle a successful save.
-	  flash[:success] = "Alarm submitted"
-	  redirect_to root_path
-	else
-	  render 'new'
+		if @alarm.save
+		  # Handle a successful save.
+		  flash.now[:success] = "Alarm submitted"
+		  redirect_to root_path
+		else
+		  render 'new'
 		end
+	end
+
+
+	def update
+		@alarm = Alarm.find(params[:id])
+		if @alarm.update_attributes(content:@alarm.content,upvotes:@alarm.upvotes+=1)
+      		# Handle a successful update.
+      		flash[:success] = "Upvoted"
+    		redirect_to alarms_path	
+    	else
+      		render 'index'
+      	end
+	end
+
+
+	def destroy
+		Alarm.find(params[:id]).destroy
+    	flash[:success] = "User deleted"
+    	redirect_to alarms_path
 	end
 
 
